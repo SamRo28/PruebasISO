e2adae249de5ec3075e0dc2f52a9b25c1b942403
Samuel Rodero Ballesteros <samuel.rodero@alu.uclm.es>
2024-11-09 12:24:38 +0100

    Comit componente 5

--- Cliente/Componente5/src/main/java/es/UCLM/esi/ISO2/C01/App.java
+++ Cliente/Componente5/src/main/java/es/UCLM/esi/ISO2/C01/App.java
@@ -6,5 +6,7 @@ package es.UCLM.esi.ISO2.C01;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
     }
 }
