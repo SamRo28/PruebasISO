390d6be3c23eae602a05a14b1c792914355f8689
Samuel Rodero Ballesteros <samuel.rodero@alu.uclm.es>
2024-11-09 12:18:16 +0100

    Prueba para el main

--- Cliente/Componente1/src/main/java/es/UCLM/esi/ISO2/C01/App.java
+++ Cliente/Componente1/src/main/java/es/UCLM/esi/ISO2/C01/App.java
@@ -6,5 +6,6 @@ package es.UCLM.esi.ISO2.C01;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Helasdfasdfas";
     }
 }
